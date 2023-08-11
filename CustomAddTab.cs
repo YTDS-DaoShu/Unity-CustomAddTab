f7e98efdf859b3aed5866fa07cb48c91ac95b490
白发青衫道沧桑 <124146201+YTDS-DaoShu@users.noreply.github.com>
2023-08-11 12:59:49 +0800

    Add files via upload

---
+++ CustomAddTab.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class CustomAddTab
+{
+    static CustomAddTab()
+    {
+        Assembly assembly = Assembly.GetAssembly(typeof(Editor));
+        Type hostView = assembly.GetType("UnityEditor.HostView");
+        FieldInfo k_PaneTypes = hostView.GetField("k_PaneTypes", BindingFlags.Static | BindingFlags.NonPublic);
+
+        k_PaneTypes.SetValue(null, new Type[]
+        {
+            typeof(SceneView),
+            typeof(TestWindow),
+            //assembly.GetType("UnityEditor.GameView"),
+            //assembly.GetType("UnityEditor.InspectorWindow"),
+            //assembly.GetType("UnityEditor.SceneHierarchyWindow"),
+            //assembly.GetType("UnityEditor.ProjectBrowser"),
+            //assembly.GetType("UnityEditor.ProfilerWindow"),
+            //assembly.GetType("UnityEditor.AnimationWindow"),
+        });
+    }
+    public class TestWindow : EditorWindow
+    {
+        public void Awake()
+        {
+            Debug.Log("CustomAddTab");
+        }
+    }
+}
